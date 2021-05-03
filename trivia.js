cb799cb691e46efaba7d612811e80005d79e7338
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2021-05-03 17:23:28 -0400

    Updated Event Listeners, Score, CSS

--- trivia.js
+++ trivia.js
@@ -5,29 +5,84 @@ let button2 = document.querySelector('.button2')
 let button3 = document.querySelector('.button3')
 let button4 = document.querySelector('.button4')
 let currentQuestion = 0
-let score = 0
+let score = document.querySelector('.score')
+let scoreNumber = 0
 
 let questions = 
 [
     {
         question: 'Gene, Louise, Tina, Bob, and Linda are the main characters of which animated sitcom?',
         choices: ['The Simpsons', 'Family Guy', 'Bob\'s Burgers', 'The Great North'],
-        answer: 2
+        answer: 'Bob\'s Burgers'
     },
     {
-        question: 'In what city would you find the infamous Alcatraz? A: San Francisco',
+        question: 'In what city would you find the infamous Alcatraz?',
         choices: ['New York', 'Detroit', 'San Jose', 'San Francisco'],
-        answer: 3
+        answer: 'San Francisco'
     },
+    {
+        question: 'Approximately how many teeth can a shark grow in a lifetime?',
+        choices: ['50,000', '500', '50', '5,000'],
+        answer: '50,000'
+    },
+    {
+        question: 'How often are brain cells replaced?',
+        choices: ['Every 30 Seconds', 'Never', 'Every Hour', 'Daily'],
+        answer: 'Never'
+    },
+    {
+        question: 'What is the national bird of the Bahamas?',
+        choices: ['The Macaw Parrot', 'The Toucan', 'The Flamingo', 'The Cockatoo'],
+        answer: 'The Flamingo'
+    },
+    {
+        question:'Bubble Wrap was invented while researchers were trying to make what?',
+        choices: ['Wallpaper', 'Styrofoam', 'Saran Wrap', 'Plastic Bags'],
+        answer: 'Wallpaper'
+    },
+    {
+        question: 'Which planet has the most gravity?',
+        choices : ['Mercury', 'Earth', 'Neptune', 'Jupiter'],
+        answer: 'Jupiter',
+    },
+    {
+        question: 'In which video game did Super Mario first appear?',
+        choices : ['Super Mario Bros.', 'Donkey Kong', 'Mario Kart', 'Yoshi\'s Island'],
+        answer: 'Donkey Kong',
+    },
+    {
+        question: 'How many hearts does an Octopus have?',
+        choices : ['3', '1', '8', '0'],
+        answer: '3',
+    },
+    {
+        question: 'What is the most consumed manufactured drink in the world?',
+        choices: ['Coca Cola', 'Tea', 'Milk', 'Coffee'],
+        answer: 'Tea'
+    }
 ]
 // console.log(questions)
 
+
 function newQuestion(){
     //set corresponding answers
-
+    // console.log(currentQuestion)
     question.innerText = questions[currentQuestion].question
+
 }
 newQuestion() 
+
+
+
+
+function newAnswers(){
+    button1.innerText = questions[currentQuestion].choices[0]
+    button2.innerText = questions[currentQuestion].choices[1]
+    button3.innerText = questions[currentQuestion].choices[2]
+    button4.innerText = questions[currentQuestion].choices[3]
+}
+newAnswers()
+
 
 function playerChoice(event) {
     //event.target.innerText 
@@ -38,34 +93,30 @@ function playerChoice(event) {
 }
 
 
-//add event listener to multiple buttons with the same id
 
-button1.addEventListener('click', (event) => {
-    currentQuestion ++
-    newQuestion()
-})   
+const answerButtons = document.querySelector('#answer-buttons')
+// console.log(answerButtons)
+answerButtons.addEventListener('click', (event) => {
+    console.log(event)
+    if (event.target.tagName === 'BUTTON') {    // checking to see if we actually clicked a button
+        if (event.target.innerText === questions[currentQuestion].answer) {    //seeing if the button we clicked was the correct answer
+            currentQuestion ++ 
+            scoreNumber += 10
+            newQuestion()
+            newAnswers()
+            score.innerText = scoreNumber
+            if (scoreNumber === 100) {
+                document.body.classList.add('Winner Winner Chicken Dinner!')
+            }
+        }
+    }
+}
 
-button2.addEventListener('click', (event) => {
-    currentQuestion ++
-    newQuestion()
-})
+)
 
-button3.addEventListener('click', (event) => {
-    score ++
-    currentQuestion ++
-    newQuestion()
-})
+//start button
 
-button4.addEventListener('click', (event) => {
-    currentQuestion ++
-    newQuestion()
-})
-
-//when an answer button is clicked - check if event.target.innerText = same as answerText
-// ++ 1 //IF correct then increment score and go to next question
-// } //ELSE do not log and move to next question ...... 
-   
-
+//reset button
 
 
 
